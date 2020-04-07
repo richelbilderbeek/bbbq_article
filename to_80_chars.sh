e8b1978fca0e44c02c2cff403f56258c1a665d37
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-07 11:18:06 +0200

    Update script

--- to_80_chars.sh
+++ to_80_chars.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# Convert article.tex to have 80 characters per line
+#
+# Usage:
+#
+#   ./to_80_chars.sh
+#
 fold -w 80 -s article.tex > article_new.tex
 cp article_new.tex article.tex
 rm article_new.tex
