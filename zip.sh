b371fcd80e3323cf3b6dbf339b64fbc8d6e0e695
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-14 12:42:28 +0200

    Zip on the things Frans needs

--- zip.sh
+++ zip.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-pandoc article.tex -o article.odt
 pandoc article.tex -o article.docx
-pandoc article.tex -o article.md
-pandoc article.tex -o article.txt
-pandoc article.tex -o article.rtf
-zip bbbq.zip article.pdf article.odt article.docx article.md article.txt article.rtf article.tex article.bib mee.bst
+zip bbbq.zip article.pdf article.docx
