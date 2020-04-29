58db3688e6d4419c9d72bada60906d72a24c605d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-29 17:00:47 +0200

    Nice pics

--- p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
+++ p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
@@ -43,53 +43,100 @@ create_random_extreme_polypeptide <- function(n_aas)
   )
 }
 
-n <- 1000
-df <- tibble::tibble(
-  hydrophobicity = rep(NA, n),
-  is_tmh = NA,
-  binds_mhc1 = NA,
-  binds_mhc2 = NA
-)
-df$hydrophobicity <- as.numeric(df$hydrophobicity)
-df$is_tmh <- as.numeric(df$is_tmh)
-df$binds_mhc1 <- as.numeric(df$binds_mhc1)
-df$binds_mhc2 <- as.numeric(df$binds_mhc2)
-for (i in seq(1, n)) {
-  polypeptide <- create_random_hydrophobic_polypeptide(n_aas = 15)
-  df$hydrophobicity[i] <- Peptides::hydrophobicity(polypeptide)
-  df$is_tmh[i] <- tmhmm::is_tmh(polypeptide)
-  df$binds_mhc1[i] <- bbbq::is_detected_by_mhc_1(polypeptide)
-  df$binds_mhc2[i] <- bbbq::is_detected_by_mhc_2(polypeptide)
+create_random_tmh <- function(n_aas)
+{
+  seq <- create_random_hydrophobic_polypeptide(n_aas)
+  while (!tmhmm::is_tmh(seq)) {
+    seq <- create_random_hydrophobic_polypeptide(n_aas)
+  }
+  seq
+}
+create_random_tmh(20)
+
+#' Create a dataset
+#' @param n number of polypeptides
+#' @param n_aas number of amino acids per polypeptide
+#' @param f function to create a polypeptide
+create_dataset <- function(
+  n,
+  n_aas,
+  f
+) {
+  df <- tibble::tibble(
+    hydrophobicity = rep(NA, n),
+    is_tmh = NA,
+    binds_mhc1 = NA,
+    binds_mhc2 = NA
+  )
+  df$hydrophobicity <- as.numeric(df$hydrophobicity)
+  df$is_tmh <- as.numeric(df$is_tmh)
+  df$binds_mhc1 <- as.numeric(df$binds_mhc1)
+  df$binds_mhc2 <- as.numeric(df$binds_mhc2)
+  for (i in seq(1, n)) {
+    polypeptide <- f(n_aas)
+    df$hydrophobicity[i] <- Peptides::hydrophobicity(polypeptide)
+    df$is_tmh[i] <- tmhmm::is_tmh(polypeptide)
+    df$binds_mhc1[i] <- bbbq::is_detected_by_mhc_1(polypeptide)
+    df$binds_mhc2[i] <- bbbq::is_detected_by_mhc_2(polypeptide)
+  }
+  df
 }
 
-ggplot(df, aes(x = hydrophobicity, y = binds_mhc1)) +
-  geom_point(height = 0.1) +
-  geom_smooth(method = "lm") +
-  labs(
-    caption = paste0(
-      "R squared: ",
-      round(
-        summary(lm(binds_mhc1 ~ hydrophobicity, data = df))$r.squared,
-        digits = 3
+
+save_plots <- function(
+  df,
+  filenames
+) {
+
+  ggplot(df, aes(x = hydrophobicity, y = binds_mhc1)) +
+    geom_point(height = 0.1) +
+    geom_smooth(method = "lm") +
+    labs(
+      caption = paste0(
+        "R squared: ",
+        round(
+          summary(lm(binds_mhc1 ~ hydrophobicity, data = df))$r.squared,
+          digits = 3
+        )
       )
-    )
-  ) + ggsave("p_bind_mhc1_per_hydrophobicity.png", width = 7, height = 7)
+    ) + ggsave(filenames[1], width = 7, height = 7)
 
-ggplot(df, aes(x = hydrophobicity, y = binds_mhc2)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  labs(
-    caption = paste0(
-      "R squared: ",
-      round(
-        summary(lm(binds_mhc2 ~ hydrophobicity, data = df))$r.squared,
-        digits = 3
+  ggplot(df, aes(x = hydrophobicity, y = binds_mhc2)) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    labs(
+      caption = paste0(
+        "R squared: ",
+        round(
+          summary(lm(binds_mhc2 ~ hydrophobicity, data = df))$r.squared,
+          digits = 3
+        )
       )
-    )
-  ) + ggsave("p_bind_mhc2_per_hydrophobicity.png", width = 7, height = 7)
+    ) + ggsave(filenames[2], width = 7, height = 7)
 
-ggplot(df, aes(x = as.factor(binds_mhc1), y = hydrophobicity, fill = as.factor(is_tmh))) +
-  geom_boxplot() + ggsave("hydrophobicity_per_mhc1.png")
+  ggplot(df, aes(x = as.factor(binds_mhc1), y = hydrophobicity)) +
+    geom_boxplot() + ggsave(filenames[3])
 
-ggplot(df, aes(x = as.factor(binds_mhc2), y = hydrophobicity, fill = as.factor(is_tmh))) +
-  geom_boxplot() + ggsave("hydrophobicity_per_mhc1.png")
+  ggplot(df, aes(x = as.factor(binds_mhc2), y = hydrophobicity)) +
+    geom_boxplot() + ggsave(filenames[4])
+}
+
+save_plots(
+  df = create_dataset(n = 1000, n_aas = 20, f = create_random_hydrophobic_polypeptide),
+  filenames = c(
+    "p_bind_mhc1_per_hydrophobicity.png",
+    "p_bind_mhc2_per_hydrophobicity.png",
+    "hydrophobicity_per_mhc1.png",
+    "hydrophobicity_per_mhc2.png"
+  )
+)
+
+save_plots(
+  df = create_dataset(n = 1000, n_aas = 20, f = create_random_tmh),
+  filenames = c(
+    "p_bind_mhc1_per_hydrophobicity_tmh.png",
+    "p_bind_mhc2_per_hydrophobicity_tmh.png",
+    "hydrophobicity_per_mhc1_tmh.png",
+    "hydrophobicity_per_mhc2_tmh.png"
+  )
+)
