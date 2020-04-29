0cf92cd9e067a6d24b8c24df5ba5520db42bb730
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-29 14:31:12 +0200

    Use 'is_tmh'

--- p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
+++ p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
@@ -46,21 +46,24 @@ create_random_extreme_polypeptide <- function(n_aas)
 n <- 1000
 df <- tibble::tibble(
   hydrophobicity = rep(NA, n),
+  is_tmh = NA,
   binds_mhc1 = NA,
   binds_mhc2 = NA
 )
+df$hydrophobicity <- as.numeric(df$hydrophobicity)
+df$is_tmh <- as.numeric(df$is_tmh)
 df$binds_mhc1 <- as.numeric(df$binds_mhc1)
 df$binds_mhc2 <- as.numeric(df$binds_mhc2)
 for (i in seq(1, n)) {
-  polypeptide <- create_random_extreme_polypeptide(n_aas = 15)
+  polypeptide <- create_random_hydrophobic_polypeptide(n_aas = 15)
   df$hydrophobicity[i] <- Peptides::hydrophobicity(polypeptide)
+  df$is_tmh[i] <- tmhmm::is_tmh(polypeptide)
   df$binds_mhc1[i] <- bbbq::is_detected_by_mhc_1(polypeptide)
   df$binds_mhc2[i] <- bbbq::is_detected_by_mhc_2(polypeptide)
 }
 
 ggplot(df, aes(x = hydrophobicity, y = binds_mhc1)) +
-  geom_jitter(height = 0.1) +
-  scale_x_continuous(limits = c(-4.5, 4.5), breaks = seq(-4.5, 4.5, by = 0.5)) +
+  geom_point(height = 0.1) +
   geom_smooth(method = "lm") +
   labs(
     caption = paste0(
@@ -73,8 +76,7 @@ ggplot(df, aes(x = hydrophobicity, y = binds_mhc1)) +
   ) + ggsave("p_bind_mhc1_per_hydrophobicity.png", width = 7, height = 7)
 
 ggplot(df, aes(x = hydrophobicity, y = binds_mhc2)) +
-  geom_jitter(height = 0.1) +
-  scale_x_continuous(limits = c(-4.5, 4.5), breaks = seq(-4.5, 4.5, by = 0.5)) +
+  geom_point() +
   geom_smooth(method = "lm") +
   labs(
     caption = paste0(
@@ -86,8 +88,8 @@ ggplot(df, aes(x = hydrophobicity, y = binds_mhc2)) +
     )
   ) + ggsave("p_bind_mhc2_per_hydrophobicity.png", width = 7, height = 7)
 
-ggplot(df, aes(x = as.factor(binds_mhc1), y = hydrophobicity)) +
+ggplot(df, aes(x = as.factor(binds_mhc1), y = hydrophobicity, fill = as.factor(is_tmh))) +
   geom_boxplot() + ggsave("hydrophobicity_per_mhc1.png")
 
-ggplot(df, aes(x = as.factor(binds_mhc2), y = hydrophobicity)) +
+ggplot(df, aes(x = as.factor(binds_mhc2), y = hydrophobicity, fill = as.factor(is_tmh))) +
   geom_boxplot() + ggsave("hydrophobicity_per_mhc1.png")
