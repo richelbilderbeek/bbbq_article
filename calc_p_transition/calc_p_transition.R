cc4c8171b54f07e687837824d12886048d5fcd22
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-30 17:02:26 +0200

    EOD

--- calc_p_transition/calc_p_transition.R
+++ calc_p_transition/calc_p_transition.R
@@ -17,42 +17,15 @@
 # * Keep the ones that result in a TMH
 # * Of those, count which state these are
 # * Convert these counts to probabilities
+library(bbbq)
 library(testthat)
 
-#' @param s the string
-#' @param n the index of the character
-#' @param c the character to be placed at that index
-replace_nth_char <- function(s, n, c) {
-  letter <- c
-  # 1: The first n-1 chars,
-  # 2: The character to be replaces
-  # 3: The rest
-  lhs <- paste0('^([A-Z]{', n - 1, '})(.)([A-Z]+)?$')
-  rhs <- paste0('\\1', letter, '\\3')
-  gsub(lhs, rhs, s)
-}
-expect_equal("XBC", replace_nth_char("ABC", 1, "X"))
-expect_equal("AXC", replace_nth_char("ABC", 2, "X"))
-expect_equal("ABX", replace_nth_char("ABC", 3, "X"))
-
-get_adjacent_sequence <- function(aa_sequence = "VVIILTIAGNILVIMAVSLE")
-{
-  seqs <- rep(NA, nchar(aa_sequence) * 20)
-  i <- 1
-  for (locus in seq(1, nchar(aa_sequence))) {
-    for (aa in bbbq::get_amino_acids()) {
-      seqs[i] <- replace_nth_char(s = aa_sequence, n = locus, c = aa)
-      i <- i + 1
-    }
-  }
-  seqs[ seqs != aa_sequence ]
-}
 
 focal_sequence <- "VVIILTIAGNILVIMAVSLE"
 expect_true(bbbq::is_detected_by_mhc_1(focal_sequence))
 expect_false(bbbq::is_detected_by_mhc_2(focal_sequence))
 
-all_seqs <- get_adjacent_sequence(focal_sequence)
+all_seqs <- get_adjacent_sequences(focal_sequence)
 length(all_seqs) #380
 
 all_tmhs <- all_seqs[ tmhmm::are_tmhs(all_seqs) ]
--- p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
+++ p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
@@ -16,21 +16,6 @@ create_random_polypeptide <- function(n_aas)
   )
 }
 
-#' Create a random hydrophobic polypeptide
-#' @param n_aas number of amino acids
-create_random_hydrophobic_polypeptide <- function(n_aas)
-{
-  aas <- bbbq::get_amino_acids()
-  hs <- Peptides::hydrophobicity(aas)
-  prob <- hs - min(hs)
-  prob <- prob * prob
-  paste0(
-    sample(
-      x = aas, size = n_aas, replace = TRUE, prob = prob),
-    collapse = ""
-  )
-}
-
 create_random_extreme_polypeptide <- function(n_aas)
 {
   aas <- bbbq::get_amino_acids()
