b9170fc2aebbd8aa24aa5ab7a30e0adae32d4229
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-20 11:30:06 +0200

    Create dataset in folder

---
+++ p_bind_per_hydrophobicity/create_dataset.R
@@ -0,0 +1,44 @@
+suppressMessages(library(ggplot2))
+suppressMessages(library(dplyr))
+
+#' Create a dataset
+#' @param n number of polypeptides
+#' @param n_aas number of amino acids per polypeptide
+#' @param f function to create a polypeptide
+create_dataset <- function(
+  n,
+  n_aas,
+  f
+) {
+  df <- tibble::tibble(
+    hydrophobicity = rep(NA, n),
+    is_tmh = NA,
+    binds_mhc1 = NA,
+    binds_mhc2 = NA
+  )
+  df$hydrophobicity <- as.numeric(df$hydrophobicity)
+  df$is_tmh <- as.numeric(df$is_tmh)
+  df$binds_mhc1 <- as.numeric(df$binds_mhc1)
+  df$binds_mhc2 <- as.numeric(df$binds_mhc2)
+  for (i in seq(1, n)) {
+    polypeptide <- f(n_aas)
+    df$hydrophobicity[i] <- Peptides::hydrophobicity(polypeptide)
+    df$is_tmh[i] <- pureseqtmr::is_tmh(polypeptide)
+    df$binds_mhc1[i] <- bbbq::is_detected_by_mhc_1(polypeptide)
+    #df$binds_mhc2[i] <- bbbq::is_detected_by_mhc_2(polypeptide)
+  }
+  df
+}
+
+df_1 <- create_dataset(
+  n = 1000,
+  n_aas = 20,
+  f = bbbq::create_random_extreme_polypeptide
+)
+df_2 <- create_dataset(
+  n = 1000,
+  n_aas = 20,
+  f = bbbq::create_random_hydrophobic_polypeptide
+)
+df <- rbind(df_1, df_2)
+write.csv(x = df, file = "p_bind_per_hydrophobicity.csv")
--- p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
+++ p_bind_per_hydrophobicity/p_bind_per_hydrophobicity.R
@@ -4,54 +4,13 @@
 # H0: there is no relation
 # H1: higher hydrophobicity equals higher chance to bind
 #
-library(ggplot2)
-library(dplyr)
+suppressMessages(library(ggplot2))
+suppressMessages(library(dplyr))
+suppressMessages(library(testthat))
 
-if (1 == 2) {
-  #' Create a dataset
-  #' @param n number of polypeptides
-  #' @param n_aas number of amino acids per polypeptide
-  #' @param f function to create a polypeptide
-  create_dataset <- function(
-    n,
-    n_aas,
-    f
-  ) {
-    df <- tibble::tibble(
-      hydrophobicity = rep(NA, n),
-      is_tmh = NA,
-      binds_mhc1 = NA,
-      binds_mhc2 = NA
-    )
-    df$hydrophobicity <- as.numeric(df$hydrophobicity)
-    df$is_tmh <- as.numeric(df$is_tmh)
-    df$binds_mhc1 <- as.numeric(df$binds_mhc1)
-    df$binds_mhc2 <- as.numeric(df$binds_mhc2)
-    for (i in seq(1, n)) {
-      polypeptide <- f(n_aas)
-      df$hydrophobicity[i] <- Peptides::hydrophobicity(polypeptide)
-      df$is_tmh[i] <- pureseqtmr::is_tmh(polypeptide)
-      df$binds_mhc1[i] <- bbbq::is_detected_by_mhc_1(polypeptide)
-      #df$binds_mhc2[i] <- bbbq::is_detected_by_mhc_2(polypeptide)
-    }
-    df
-  }
 
-  df_1 <- create_dataset(
-    n = 1000,
-    n_aas = 20,
-    f = bbbq::create_random_extreme_polypeptide
-  )
-  df_2 <- create_dataset(
-    n = 1000,
-    n_aas = 20,
-    f = bbbq::create_random_hydrophobic_polypeptide
-  )
-  df <- rbind(df_1, df_2)
-  write.csv(x = df, file = "~/raw_data.csv")
-}
-
-raw_data <- system.file("extdata", "raw_data.csv", package = "bbbq")
+raw_data <- "p_bind_per_hydrophobicity.csv"
+expect_true(file.exists(raw_data))
 
 plot_is_tmh_vs_binds_mhc1 <- function(
   df = read.csv(system.file("extdata", "raw_data.csv", package = "bbbq")),
