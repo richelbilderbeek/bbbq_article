ae95fb5f02874753caa7a83052a21c84a0fc9a07
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-21 09:26:33 +0200

    Add 'peregrine_make.sh'

---
+++ p_bind_per_hydrophobicity/peregrine_make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# make this project on Peregrine, by loading the required modules first.
+#
+# Usage (in queue, preferred):
+#
+#   sbatch ./peregrine_make.sh
+#
+# Usage (local):
+#
+#   ./peregrine_make.sh
+#
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=peregrine_make
+#SBATCH --output=peregrine_make.log
+module load Pandoc
+make
