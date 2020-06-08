3d1225eb2dac32608fb8004097bb157366206eb3
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-08 10:57:27 +0200

    Better log name

--- p_bind_per_hydrophobicity/make.sh
+++ p_bind_per_hydrophobicity/make.sh
@@ -10,8 +10,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=10G
-#SBATCH --job-name=make_p_bind_per_hydrophobicity
-#SBATCH --output=make_p_bind_per_hydrophobicity.log
+#SBATCH --job-name=p_bind_per_hydrophobicity
+#SBATCH --output=p_bind_per_hydrophobicity.log
 module load R Python/3.8.2-GCCcore-9.3.0 binutils ImageMagick X11 libX11 xprop
 make
 
