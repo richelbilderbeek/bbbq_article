3f0537786057eb284109dfd0eea26720fadd2777
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-08 10:46:50 +0200

    Use 20 GB

--- ep_vs_mhcn/make.sh
+++ ep_vs_mhcn/make.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
+#SBATCH --mem=20G
 #SBATCH --job-name=make_data_ep_vs_mhcn
 #SBATCH --output=make_data_ep_vs_mhcn.log
 module load R Python/3.8.2-GCCcore-9.3.0 binutils ImageMagick X11 libX11 xprop
