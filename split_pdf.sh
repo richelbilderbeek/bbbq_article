bfe5a3fa86a31084b6b656b327dff735877dfb46
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-11 08:16:14 +0200

    PDF with figures

--- split_pdf.sh
+++ split_pdf.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-# From the PDF 'bbbq_article.pdf', create two new files:
+# From the PDF 'bbbq_article.pdf', create these new files:
 #
 # (1) 'bbbq_article_main.pdf', the paper,  
-# (2) 'bbbq_article_supplementary_materials.pdf', the supplementary materials
+# (2) 'bbbq_article_figures.pdf', the figures of the main part,  
+# (3) 'bbbq_article_supplementary_materials.pdf', the supplementary materials
 #
 # usage:
 #
 #   ./split_pdf.sh
 #
-pdftk bbbq_article.pdf cat 1-31 output bbbq_article_main.pdf
-pdftk bbbq_article.pdf cat 32-54 output bbbq_article_supplementary_materials.pdf
+pdftk bbbq_article.pdf cat 1-27 output bbbq_article_main.pdf
+pdftk bbbq_article.pdf cat 29-32 output bbbq_article_figures.pdf
+pdftk bbbq_article.pdf cat 33-55 output bbbq_article_supplementary_materials.pdf
