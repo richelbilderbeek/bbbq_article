bfea97edf51e1cff95a405650f8deb99299a7447
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-27 17:13:26 +0200

    EOD

--- create_numbered_figure_files.R
+++ create_numbered_figure_files.R
@@ -1,4 +1,5 @@
-#
+# Create the numbered figures as 'figure_1.tiff' etc,
+# from the articles' better-described figure names
 
 tex_lines <- readr::read_lines("bbbq_article.tex")
 includegraphics_lines <- stringr::str_subset(tex_lines, "^  .*includegraphics")
@@ -9,3 +10,13 @@ tiff_filenames <- stringr::str_replace(png_filenames, "png$", "tiff")
 tiff_filenames[!file.exists(tiff_filenames)]
 testthat::expect_true(all(file.exists(tiff_filenames)))
 
+t <- tibble::tibble(
+  from = tiff_filenames,
+  to = NA
+)
+t$to[1:4] <- paste0("~/figure_", seq(1, 4), ".tiff")
+t$to[5:nrow(t)] <- paste0("~/figure_s", seq(1, nrow(t) - 4), ".tiff")
+
+for (i in seq_len(nrow(t))) {
+  file.copy(t$from[i], t$to[i])
+}
