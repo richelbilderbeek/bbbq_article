fdb69d9f81c403645b5ab23741b2941f2a225a30
richelbilderbeek <richel@richelbilderbeek.nl>
2021-12-12 17:36:15 +0100

    First step in supmat

---
+++ create_frontiers_supmat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Create the PDF 'frontiers_supmat.pdf'
+#
+# It does so by converting the TeX to PDF, 
+# as well as stitching some other PDFs to it
+#
+# Usage:
+#
+#   ./create_frontiers_supmat.sh
+#
+pdflatex frontiers_SupplementaryMaterial.tex
+bibtex frontiers_SupplementaryMaterial.tex >/dev/null
+pdflatex frontiers_SupplementaryMaterial.tex >/dev/null
+pdflatex frontiers_SupplementaryMaterial.tex
+mv frontiers_SupplementaryMaterial.pdf frontiers_supmat.pdf
+
